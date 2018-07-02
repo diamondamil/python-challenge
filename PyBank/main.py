a3e437302d35551cfe985ab93832be14f7dee055
diamondamil <37598428+diamondamil@users.noreply.github.com>
2018-07-02 00:12:42 -0700

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,91 @@
-Create
+import os
+import csv
+
+csvpath = os.path.join("budget_data.csv")
+
+with open(csvpath, newline="") as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+
+
+    csv_header = next(csvreader)
+
+#list and calculate the summary numbers
+
+    totalmonth = 0
+
+    totalamount = 0.0
+
+    Month = []
+
+    Revenue =[]
+
+    changes = [ ]
+
+    different = 0.0
+
+
+
+    for row in csvreader:
+
+        
+
+        totalmonth += 1
+
+        totalamount += float(row[1])
+
+        changes.append (- different + float(row[1]))
+
+        Month.append(row[0])
+
+        Revenue.append(row[1])
+
+        different = float(row[1])
+
+    changes.pop(0)
+
+    
+    totalchange = 0.0
+
+    numberofchange = 0
+
+#get the average change
+
+    for change in changes:
+
+        totalchange += change
+
+        numberofchange +=1
+
+    avgchange = totalchange / numberofchange
+
+#get the (fx)max and (fx)mix changes in profit and position in the list
+
+    maxchange = max(changes)
+
+    minchange = min(changes)
+
+    maxmonth = Month[changes.index(maxchange)+1]
+
+    minmonth = Month[changes.index(minchange)+1]
+
+    maxrevenue = Revenue[changes.index(maxchange)+1]
+
+    minrevenue = Revenue[changes.index(minchange)+1]
+
+
+
+    print ("Financial Analysis")
+
+    print("-----------------------------------")
+
+    print("Total Months: "+ str(totalmonth))
+
+    print("Total: "+ "$"+str(int(totalamount)))
+
+    print("Average Change: $"+str("{0:.2f}".format(avgchange)))
+
+    print("Greatest Increase in Profits:"+ maxmonth + " ($"+ str(int(maxchange)) + ")")
+
+    print("Greatest Decrease in Profits:"+ minmonth + " ($"+ str(int(minchange)) + ")")
