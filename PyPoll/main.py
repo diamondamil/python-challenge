17d2872b03744af381e4cdcb7e128a48dc9b125e
diamondamil <37598428+diamondamil@users.noreply.github.com>
2018-07-07 16:38:09 -0700

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,90 +1,198 @@
 
 import os
+
 import csv
 
-ElectData = os.path.join("Resources", "election_data2.csv")
-
-totalnumber = 0
-
-canvote = 0
-
-candidatelist = {}
 
 
-with open(ElectData, newline="") as ElectData:
+## Use the os module to read file path
 
-    csvreader = csv.reader(ElectData, delimiter=",")
+csv_Path = os.path.join("Resources", "election_data.csv")
 
-    header = next(ElectData)
+# print(csv_Path)
 
 
 
-    for pypoll in csvreader:
+# Total Votes counter
 
-        totalnumber = totalnumber +1  
+TotalVotes = 0
 
-        
+WinningVoteCount = 0
 
-        if pypoll[2] in candidatelist:
+WinningCandidate = ""
 
-           candidatelist[pypoll[2]] += 1
-
-        else:
-
-           candidatelist[pypoll[2]] = 1
+WinningPercent = 0.00
 
 
 
-    print("------------------------------------------------------------------")    
+# Create lists 
 
-    print("Total Votes: " + str(totalnumber))
+CandidatesWithVotes = []
 
-    print("------------------------------------------------------------------")
+CandidateVotes = {}
+
+
+
+
+
+## Open the CSV
+
+with open(csv_Path, newline = "") as csv_File:
+
+
+
+  # Initiate csv reader
+
+  csv_Reader = csv.reader(csv_File, delimiter = ",")
+
+  # print(csv_Reader)
+
+
+
+  # Read Header row
+
+  csv_Header = next(csv_File)
+
+  # print(f'\nHeader: {csv_Header}')
+
+
+
+  # Read each row of data
+
+  for x in csv_Reader:
+
+
+
+    # Add to the total votes
+
+    TotalVotes = TotalVotes + 1
+
+
+
+    # Extract candidate name 
+
+    CandidateName = x[2]
+
+   
+
+    # If Candidate not in existing Candidate With Votes list...
+
+    if CandidateName not in CandidatesWithVotes:
+
+
+
+      # Add Candidate to the list
+
+      CandidatesWithVotes.append(CandidateName)
+
+
+
+      # Track Candidate votes
+
+      CandidateVotes[CandidateName] = 0
 
     
 
-    maxvote = max(candidatelist.values())
+    # Add a vote to the Candidates count
 
-    for canvote in candidatelist:
-
-        voterate = candidatelist[canvote] / totalnumber * 100
-
-        if candidatelist[canvote] == maxvote:
-
-           x = canvote
-
-        print(canvote + ": " + "{0:.3f}".format(voterate) + "% " + "(" + str(candidatelist[canvote]) + ")")
-
-    print("------------------------------------------------------------------") 
-
-    print("Winner: " + x)
-
-
-for name in clean_data:
-
-    if max(num_votes) == name[1]:
-
-        winners_list.append(name[0])
-
-output_file = os.path.join('Output', 'election_results_' + str(file_num) +'.txt')
-
-
-print("Election Results")
-
-print("--------------------------")
-
-print("Total Votes: " + str(total_votes))
-
-print("--------------------------")
+    CandidateVotes[CandidateName] = CandidateVotes[CandidateName] + 1
 
 
 
 
 
-print("Winner: " + winner)
+    # Loop through the candidates to determine winner
+
+    for y in CandidateVotes:
+
+      
+
+      # Determine vote counts and percentages
+
+      Votes = CandidateVotes.get(CandidateName)
+
+      VotePercent = (float(Votes) / float(TotalVotes)) * 100
 
 
 
-with open(output_file, 'r') as readfile:
+      # Determine the Winner
 
-    print(readfile.read())
+      if Votes > WinningVoteCount:
+
+        WinningVoteCount = Votes
+
+        WinningCandidate = CandidateName
+
+        WinningPercent = round((WinningVoteCount / TotalVotes) * 100 , 2)
+
+
+
+  # Loop through candidates and print votes and percentage
+
+  for k, v in CandidateVotes.items():
+
+    Percent = (v / TotalVotes) * 100
+
+    print(k , v , str(round(Percent,2)) + "%")
+
+    
+
+
+
+# Print Output  
+
+print(f'\nTotal Votes: {TotalVotes}')
+
+print(f'\nWinner: {WinningCandidate} with {WinningVoteCount} votes ({WinningPercent}%).')
+
+
+
+
+
+# Use os module to specify output file to WRITE to
+
+csv_Output_Path = os.path.join("PyPollResults.txt")
+
+
+
+# Open the output file using WRITE mode
+
+with open(csv_Output_Path, "w", newline = "") as csv_File_Out:
+
+
+
+    # Initialize csv.writer
+
+    csv_Writer = csv.writer(csv_File_Out)
+
+
+
+    # Write results to text file
+
+    csv_Writer.writerow(["Results"])
+
+    csv_Writer.writerow(["----------"])
+
+    csv_Writer.writerow(["Total Votes: " + str(TotalVotes)])
+
+    csv_Writer.writerow(["----------"])
+
+
+
+    # Loop through candidates and print votes and percentage
+
+    for k, v in CandidateVotes.items():
+
+      Percent = (v / TotalVotes) * 100
+
+      csv_Writer.writerow([k , v, str(round(Percent,2)) + "%"]) 
+
+
+
+    # Output winner
+
+    csv_Writer.writerow(["----------"])
+
+    csv_Writer.writerow(["Winner: " + WinningCandidate + " with " + 
+
+                          str(WinningVoteCount) + " votes (" + str(WinningPercent) + "%)."])
